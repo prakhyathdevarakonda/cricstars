aeceaee778a74f865874f34a99dd55e0bc96fecf
White-Jaguar <93778576+White-Jaguar@users.noreply.github.com>
2022-10-28 13:29:46 +0530

    Add files via upload

---
+++ opening.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+<form name="createm" method="post" action="scorer.php">
+Striker<input type="text" name="striker"><br>
+Non-Striker<input type="text" name="nstriker"><br>
+Opening Bowler<input type="text" name="obowler"><br>
+<input type="submit" value="Start Match">
+</form>
+</body>
+</html>
---
+++ scorer.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="refresh" content="5" />
+	<title> Live Scores </title>
+	<link   rel="stylesheet" href="css/style.css" type="text/css"> </link>
+	
+</head>
+<body>
+<div>
+	<header>
+		<h1>Live Scores</h1>
+	</header>
+	<nav>
+	</nav>
+</div>
+</body>
+</html>
