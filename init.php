9c3c49f8ad7ff06ff688fa73a5c1a6eafacb1205
White-Jaguar <93778576+White-Jaguar@users.noreply.github.com>
2022-11-14 23:31:44 +0530

    files upload

---
+++ init.php
@@ -0,0 +1,12 @@
+<?php
+
+
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'cricstars');
+
+spl_autoload_register(function($class_name) {
+	require $_SERVER['DOCUMENT_ROOT'] . '/cricstars/' . $class_name . '.php';
+});
+?>
